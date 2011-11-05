3457fcd7ad6e46d99295107ad7e287c5157b212d
Bauwe Bijl <bauwebijl@gmail.com>
2011-11-05 12:50:21 +0100

    modified atomfeed with control option:   views/atom/map.js

--- views/atom/map.js
+++ views/atom/map.js
@@ -1,6 +1,8 @@
 function(doc) {
-	if(doc.title && doc.tags == "content") {
+//full atom control: in newtiddler macro ->fields:"atom:publish"<-// 
+//	if(doc.fields.atom == "publish") {
+//"creator" is the unique key for tiddlers created after couchapp deployment//
+	if(doc.creator) {
 	emit(new Date(doc.created), doc);
 	}
 }
-
